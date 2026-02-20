21877a0240d1cdce80b2e641d8fbf851fc76376f
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-02-20 12:26:54 +0100

    Feat: Frontend: Added all new components on landing page

--- frontend/src/app/shared/home/home.component.ts
+++ frontend/src/app/shared/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
+import { ValueComponent } from '../value/value.component';
+import { HowItWorkComponent } from '../how-it-work/how-it-work.component';
+import { ImpactComponent } from '../impact/impact.component';
+import { DemoComponent } from '../demo/demo.component';
+import { ReviewComponent } from '../review/review.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HowItWorkComponent, ValueComponent, ImpactComponent, DemoComponent, ReviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
