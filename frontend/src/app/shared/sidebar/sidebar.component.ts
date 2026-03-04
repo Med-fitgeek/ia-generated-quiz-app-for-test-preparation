f8d0af990e6ed3a5bab0d4ccca4113fcf3f0588e
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-04 12:31:41 +0100

    Feat: Frontend: added logout button to sidebar

--- frontend/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
 
+  isAuthenticated$ = this.authService.isAuthenticated$;
+  
+    constructor(private authService: AuthService) {}
+  
+    logout(): void {
+      this.authService.logout();
+    }
 }
