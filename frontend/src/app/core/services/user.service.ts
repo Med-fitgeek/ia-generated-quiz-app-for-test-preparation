ff0e52ce503a2295af5cc4b432a9c9bebc3cbb5d
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-22 16:17:15 +0100

    Completed user services methodes

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { User } from '../models/user-dto.model';
+import { UpdatePasswordRequest } from '../models/update-password-request.model';
+import { UpdateProfileRequest } from '../models/update-profil-request.model';
 
 @Injectable({ providedIn: 'root' })
 
@@ -22,6 +24,18 @@ export class UserService {
 
     return this.http.put<User>(`${this.apiUrl}/users/me`, data);
 
+  }
+
+   updateProfile(data: UpdateProfileRequest): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/me`, data);
+  }
+ 
+  updatePassword(data: UpdatePasswordRequest): Observable<void> {
+    return this.http.patch<void>(`${this.apiUrl}/users/me/password`, data);
+  }
+ 
+  deleteAccount(): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/users/me`);
   }
 
 }
