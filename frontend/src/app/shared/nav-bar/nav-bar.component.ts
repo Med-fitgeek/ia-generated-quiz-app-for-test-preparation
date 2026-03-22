89ea5c48dec305f10c064fb9d43532c4bb61f8cc
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-03-22 16:10:06 +0100

    Updated navbar style

--- frontend/src/app/shared/nav-bar/nav-bar.component.ts
+++ frontend/src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
+import { UserService } from '../../core/services/user.service';
+import { AvatarService } from '../../core/services/avatar.service';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,32 +13,61 @@ import { Observable } from 'rxjs';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
-
-  menuOpen = false;
+export class NavBarComponent implements OnInit {
 
   isAuthenticated$: Observable<boolean>;
-  isOpen = false;
+  isMenuOpen = false;
+  dropdownOpen = false;
 
-  constructor(private authService: AuthService) {
+  userName = '';
+  userEmail = '';
+  userAvatarId = 'a1';
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    readonly avatarService: AvatarService
+  ) {
     this.isAuthenticated$ = this.authService.isAuthenticated$;
   }
-  
-  
-  isMenuOpen = false;
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+  ngOnInit(): void {
+    this.isAuthenticated$.subscribe(auth => {
+      if (auth) {
+        this.userService.getCurrentUser().subscribe(user => {
+          this.userName     = user.username;
+          this.userEmail    = user.email;
+          this.userAvatarId = user.avatarId ?? 'a1';
+        });
+      }
+    });
   }
 
-  close(): void {
-    this.isOpen = false;
+  get userAvatar() {
+    return this.avatarService.getById(this.userAvatarId);
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) this.dropdownOpen = false;
+  }
+
+  closeMenu(): void { this.isMenuOpen = false; }
+
+  toggleDropdown(): void { this.dropdownOpen = !this.dropdownOpen; }
+
+  closeDropdown(): void { this.dropdownOpen = false; }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (!(event.target as HTMLElement).closest('.avatar-menu')) {
+      this.dropdownOpen = false;
+    }
   }
 
   logout(): void {
     this.authService.logout();
-    this.close();
+    this.closeMenu();
+    this.closeDropdown();
   }
-
-
 }
